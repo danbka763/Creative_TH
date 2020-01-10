a8eea92dee35337374eac8bbe9a6be5c81b1d747
danbka763 <48606033+danbka763@users.noreply.github.com>
2020-01-10 20:15:43 +0700

    adaptive
    adaptive s1 for mobile

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ function header_adaptive_size(){
   if ($(window).width() <= 420) {
     $('.logo-backgr-blue').width($(window).width()-55);
     $('.logo-backgr-blue').height(($('.logo-backgr-blue').width()/2));
+
+    
     
     $('.img-logo').width($(window).width()-256);
   }
@@ -37,14 +39,26 @@ function header_adaptive_size(){
   };
 };
 
-let firstTop_blue = parseInt($('.form-blue-big').css('top'))
 // Зависимость высоты от ширины форм в s1
+// + местоположение полигона blue относительно black
+// let firstTop_blue = parseInt($('.form-blue-big').css('top'))
+
 function adaptive_size_polygon(){
     $('.form-black').height($('.form-black').width());
-    $('.form-blue-big').height($('.form-blue-big').width());
+    if ($(window).width() <= 680) {
+      $('.form-blue-big').width($('.form-black').width()/2);
+      $('.form-blue-big').height($('.form-black').height());
+    }
+    else {
+      $('.form-blue-big').width(500);
+      $('.form-blue-big').height(500);
+    }
 
-    let top = firstTop_blue - 284 + $('.form-black').height()/2;
-    $('.form-blue-big').css('top', top);
-
-    console.log($('.form-blue-big').css('top'))
-}
+    // let top = firstTop_blue - 284 + $('.form-black').height()/2;
+    if ($(window).width() <= 680) {
+      $('.form-blue-big').css('top', function(){
+      return parseInt($('.form-black').css('top')) + 
+                        $('.form-black').width() / 2;
+      });
+    }
+};
